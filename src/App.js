9f54d6fbcc62edc1c262ec58dab20bd507733ed2
Piotr Lachowicz <piotr.lachowicz@strike.co.uk>
2022-06-08 11:41:10 +0100

    cognito set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Auth } from "aws-amplify";
+import {useState} from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  async function getUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log(user);
+    setToken(user.signInUserSession.idToken.jwtToken)
+  }
+
+  function getViewings() {
+    fetch('https://admin.local.strike.dev/api/appointments/viewings', {
+      headers: { "Authorization": token ?? '' }
+    }).then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+            onClick={ () => Auth.federatedSignIn({ provider: "Google" }) }
+        >Sign with google
+        </button>
+
+        <button onClick={getUser} >get cognito user</button>
+
+        <button onClick={getViewings} >get viewings</button>
+
       </header>
     </div>
   );
